2d36f82864c460998a0da3d7be277643385292f6
m-fhm <joinfa333@yahoo.com>
2021-06-15 19:16:38 +0500

    sorted list and update

--- script.py
+++ script.py
@@ -1,22 +1,24 @@
-"""Parse weight and parents fields; What i understand is to write those transactions which have parents from mempool to block.txt """
-def parse_mempool_csv():
-    """Parse the CSV file and return a list of MempoolTransactions."""
-    with open('mempool.csv') as mempool:
-        return([line.strip().split(',') for line in mempool.readlines()])
+import pandas as pd
+df=pd.read_csv('mempool.csv')
+df=df.sort_values(by = 'fee',ascending = False)
+transactions=df.values.tolist()
 try:
-    transations = parse_mempool_csv()
     out_data = open("block.txt",'a')# open block.txt file to write transations as output
     block_weight=0
-    for transation in  transations[1:]:
-        if transation[3] !='': #check if the transation have parent or not transaction[3] pick the parents of transation
-            block_weight= block_weight + int(transation[2]) #calculate the block weight
-            if block_weight <= 4000000: 
-                transation_parents= transation[3].split(';') #collect the parents in transation_parents list
-            for item in transation_parents: #pick the parent from list of parents
+    tracked_transactions= []
+    for transaction in transactions:
+        if block_weight <= 4000000:
+            if type(transaction[3]) is not str: #check if the transation have parent or not transaction[3] pick the parents of transation
+                out_data.write(transaction[0])
                 out_data.write("\n")
-                out_data.write(item) #write the parent before transation 
-            out_data.write("\n")  
-            out_data.write(transation[0]) #write the transation at end of parents
+                block_weight= block_weight + transaction[2] #calculate the block weight
+            else:            
+                transation_parents= transaction[3].split(';')
+                check =  all(item in tracked_transactions  for item in transation_parents)
+                if check is True:
+                    out_data.write(transaction[0])
+                    out_data.write("\n")
+                    block_weight= block_weight + transaction[2] #calculate the block weight
 except:
     print("generic error")
 else:
